fb5dcb99428f9e0de661584778f2df1c2997c5a9
ichraf khalfaoui <ichrafkhalfaoui1@gmail.cm>
2024-01-03 14:21:44 +0100

    sample commit

--- backend/server.js
+++ backend/server.js
@@ -28,23 +28,22 @@ app.use('/api/upload', uploadRoutes);
 app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
-
-const __dirname = path.resolve();
-
+const rootDirectory = path.resolve();
 if (process.env.NODE_ENV === 'production') {
-  app.use('/uploads', express.static('/var/data/uploads'));
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(rootDirectory, 'uploads')));
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
 } else {
+  const __dirname = path.resolve();
   app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
   app.get('/', (req, res) => {
     res.send('API is running....');
   });
 }
-
 
 app.use(notFound);
 app.use(errorHandler);
